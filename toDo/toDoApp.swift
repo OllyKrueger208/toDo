22534d3d85d26b118dcd446cf5fea113ee0f8a2a
Olly <makrueger208@gmail.com>
2023-06-27 10:45:17 -0600

    Initial Commit

---
+++ toDo/toDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoApp.swift
+//  toDo
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
